b06036b80b8135bf855af252c18cdef6d9160a3e
Patryk Petryszen <p.petryszen@gmail.com>
2021-01-29 08:47:26 +0000

    flask_web

---
+++ web_server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/') #root directory
+def main_page():
+    return render_template('index.html')
+    #When we use render_template flask is automatically looking for 'templates' folder
+    
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted hoooraaaay'
+
+# @app.route('/favicon.ico')
+# def blog():
+#     return 'This will be my blog'
